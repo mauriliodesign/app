f3092766e3f256b667108bd3f9e45bbb7ec864e2
Toshiuk <nara_toshiraka@hotmail.com>
2022-10-05 01:19:26 +0100

    mapinha

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react"
-import { Wrapper, Status } from "@googlemaps/react-wrapper"
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import Map from "./components/Map";
+import "./styles.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const render = (status: Status) => {
-    return <h1>{status}</h1>
-  }
+    const render = (status: Status) => {
+        return <h1>{status}</h1>;
+    };
 
-  return (
-    <div className="App">
-      <Wrapper apiKey={"YOUR_API_KEY"} render={render}>
-        <YourComponent />
-      </Wrapper>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Wrapper apiKey={"AIzaSyDENelulmyQiu72laCFfCs7Kwa1ARKMDLc"} render={render}>
+                <Map />
+            </Wrapper>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,22 @@
-import { useState } from "react"
-import { Wrapper, Status } from "@googlemaps/react-wrapper"
+import { useEffect, useRef, useState } from "react";
 
+const Map = () => {
+    const ref = useRef();
+    const center = { lat: -34.397, lng: 150.644 };
+    const zoom = 4;
 
-export default function Map() {
-    const render = (status: Status) => {
-      return <h1>{status}</h1>
-    }
-  return (
-    <div>Map</div>
-  )
-}
+    const [map, setMap] = useState();
+
+    useEffect(() => {
+        if (ref.current && !map) {
+            setMap(new window.google.maps.Map(ref.current, {}));
+        }
+        if (map) {
+            map.setOptions({ zoom: 11, center: { lat: 33.669445, lng: -117.823059 } });
+        }
+    }, [ref, map]);
+
+    return <div ref={ref} id="map" style={{ height: "100%", width: "100%", margin: "auto" }} />;
+};
+
+export default Map;
