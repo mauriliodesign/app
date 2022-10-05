27ed4d99d1e83cde23466890ad47bc5773309227
Toshiuk <nara_toshiraka@hotmail.com>
2022-10-05 01:56:53 +0100

    alterações

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
                     <Map />
                 </Wrapper>
             </div>
+            <button type="submit">Criar rolê</button>
         </div>
     );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,15 +3,29 @@ import { useEffect, useRef, useState } from "react";
 const Maps = () => {
     const ref = useRef<HTMLDivElement>(null);
     const [map, setMap] = useState<google.maps.Map>();
+    const [lat, setLat] = useState<number>(33.669445);
+    const [lng, setLng] = useState<number>(-117.823059);
+
+    useEffect(() => {
+        navigator.permissions.query({ name: "geolocation" });
+        navigator.geolocation.getCurrentPosition((geolocation) => {
+            const { latitude, longitude } = geolocation.coords;
+            setLat(latitude);
+            setLng(longitude);
+        });
+    }, []);
 
     useEffect(() => {
         if (ref.current && !map) {
             setMap(new window.google.maps.Map(ref.current, {}));
         }
+    }, [ref]);
+
+    useEffect(() => {
         if (map) {
-            map.setOptions({ zoom: 11, center: { lat: 33.669445, lng: -117.823059 } });
+            map.setOptions({ zoom: 11, center: { lat, lng } });
         }
-    }, [ref, map]);
+    }, [lat, lng, map]);
 
     return <div ref={ref} id="map" style={{ height: "100%", width: "100%" }} />;
 };
